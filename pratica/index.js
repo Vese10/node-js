54ba9c34282b1f6c2173a73acb5e5da15852fa0d
Vese10 <146728778+Vese10@users.noreply.github.com>
2024-03-03 10:00:40 +0100

    query param search and limit

--- pratica/index.js
+++ pratica/index.js
@@ -63,8 +63,157 @@
         client --> richiede una pagina (es. pagina personale dopo Login) --> server --> renderizza pagina completa html --> cliente la mostra a schermo
 */
 
-/* Usare JSON per passare i dati:
+/* Usare JSON per passare i dati: Express Json
+        Mandiamo come risposta dei dati con json in linea:
+            const express = require('express')
+            const app = express()
 
+            app.get('/', (req, res) => {
+                res.json([{nome: "Luca", cognome: "Rossi"}, {nome: "Anna", cognome: "Neri"}]) //La risposta sarà un file json, in questo caso composto da un array di oggetti dichiarati in linea.
+            })
+
+            app.listen(3000);
+        
+        Mandiamo come risposta dei dati con json da file esterno.
+            const express = require('express')
+            const app = express()
+            const {persone} = require('./persone') // Importiamo il file js persone per poterlo poi utilizzare come risposta.
+
+            app.get('/', (req, res) => {
+                res.json(persone) //La risposta sarà un file json ma esterno, non in linea. Lo abbiamo importato sopra.
+            })
+
+            app.listen(3000);
 */
 
-         
+/* Route Param: 
+        - Come inserire solo alcuni specifici parametri di risposta attraverso il mapping:
+            const express = require('express')
+            const app = express()
+            const {persone} = require('./persone') // Importiamo il file js persone per poterlo poi utilizzare come risposta.
+
+            app.get('/', (req, res) => {
+                res.send("<h1>Homepage</h1><a href='/persone'>Vai a persone</a>") // La risposta su localhost:3000
+            })
+
+            app.get('/persone', (req, res) => {
+                const nuovePersone = persone.map((persona) => { //Dichiariamo la nuova variabile che vogliamo come risposta
+                    const {nome, cognome, eta} = persona // Inseriamo i parametri specifici che vogliamo (nome, cognome, eta)
+                    return {nome, cognome, eta} 
+                })
+                res.json(nuovePersone)  //La risposta su localhost:3000/persone sono i dati ottenuti dal file "persone";
+            })
+
+            app.listen(3000);
+
+        - Come richiedere dati di un oggetto specifico (hardcode):
+            const express = require('express')
+            const app = express()
+            const {persone} = require('./persone') // Importiamo il file js persone per poterlo poi utilizzare come risposta.
+
+            app.get('/', (req, res) => {
+                res.send("<h1>Homepage</h1><a href='/persone'>Vai a persone</a>") // La risposta su localhost:3000
+            })
+
+            app.get('/persone', (req, res) => {
+                const nuovePersone = persone.map((persona) => { //Dichiariamo la nuova variabile che vogliamo come risposta
+                    const {nome, cognome, eta} = persona // Inseriamo i parametri specifici che vogliamo (nome, cognome, eta)
+                    return {nome, cognome, eta} 
+                })
+                res.json(nuovePersone)  //La risposta su localhost:3000/persone sono i dati ottenuti dal file "persone";
+            })
+
+            app.get('/persone/1', (req, res) => { // Per url localhost:3000/persone/1
+                const persona = persone.find((persona) => { // vogliamo ottenere come risposta la variabile persona specifica
+                    return persona.id === '1' // avente id = 1
+                })
+                res.json(persona)
+            })
+
+            app.get('/persone/2', (req, res) => { // Per url localhost:3000/persone/2
+                const persona = persone.find((persona) => { // vogliamo ottenere come risposta la variabile persona specifica
+                    return persona.id === '2' // avente id = 2
+                })
+                res.json(persona)
+            })
+
+            app.listen(3000);
+
+        - L'esempio sopra non è però utilizzabile per file json di risposta ampi, se abbiamo un numero illimitato di oggetti non possiamo scrivere a mano la risposta per ogni oggetto. Qui entrano in gioco i ROUTE PARAM e cioè un modo di scrivere il codice universale valido per tutti gli oggetti del json:
+            const express = require('express')
+            const app = express()
+            const {persone} = require('./persone') // Importiamo il file js persone per poterlo poi utilizzare come risposta.
+
+            app.get('/', (req, res) => {
+                res.send("<h1>Homepage</h1><a href='/persone'>Vai a persone</a>") // La risposta su localhost:3000
+            })
+
+            app.get('/persone', (req, res) => {
+                const nuovePersone = persone.map((persona) => { //Dichiariamo la nuova variabile che vogliamo come risposta
+                    const {nome, cognome, eta} = persona // Inseriamo i parametri specifici che vogliamo (nome, cognome, eta)
+                    return {nome, cognome, eta} 
+                })
+                res.json(nuovePersone)  //La risposta su localhost:3000/persone sono i dati ottenuti dal file "persone";
+            })
+
+            app.get('/persone/:id', (req, res) => { // Per url localhost:3000/persone/:id --> in base al numero che inseriamo al posto di id avremo la risposta con l'oggetto avente tale id.
+                const personaID = req.params.id // alla richiesta dell'oggetto con l'id inserito
+                const persona = persone.find((persona) => { //vogliamo ottenere in risposta l'oggetto' specifico avente quel id
+                    return persona.id === personaID 
+                })
+                if(!persona) { // se inseriamo un id che non esiste nel nostro file di risposta la persona non esiste:
+                    return res.status(404).send("Persona non trovata") // restituisci l'errore qui definito
+                }
+                res.json(persona)
+            })
+
+            app.listen(3000);
+*/
+
+/* Query String Param (url param):
+        Search: Per ottenere come risposta dei dati specifici ricercati inseriti nell'url usiamo la seguente scrittura nell'url:
+        es. localhost:3000/persone/search?nome=luca&cognome=rossi
+
+            const express = require('express')
+            const app = express()
+
+            app.get('/persone/search', (req, res) => {
+                console.log(req.query) // alla richiesta di un particolare url (es. localhost:3000/persone/search?nome=luca&cognome=rossi) manda in console i dati richiest nell'url stesso;
+                res.send("ciao")
+            })
+
+            app.listen(3000); 
+
+        Limit: 
+            const express = require('express')
+            const app = express()
+            const {persone} = require('./persone')
+
+            app.get('/persone/search', (req, res) => {
+                const {query, limit} = req.query // gestiamo i parametri query e limit che ci arrivano dalla query (url dopo il search);
+                let personeFiltrate = [...persone] // impostiamo la variabile che in questo caso corrisponde al nostro file json facendone una copia (in modo da non intaccare il file originale se dovessimo volerlo modificare da qui);
+
+                if(query){ // se query esiste e cioè se abbiamo effettivamente dei valori di ricerca
+                    personeFiltrate = personeFiltrate.filter((persona) => { // filtriamo i nostri parametri
+                        return persona.nome.startsWith(query) // il filtro è il nome della persona che inizia con la nostra query (es. localhost:3000/search?query=D)
+                    })
+                }
+
+                if(limit){ //impostiamo il limite massimo di risultati che vogliamo ottenere con la nostra query (se i risultati totali sono 100 ma noi abbiamo impostato un limit di 20, otterremo le prime 20 risposte). url esempio: localhost:3000/search?query=D&limit=1
+                    personeFiltrate = personeFiltrate.slice(0, Number(limit)) //tramite slice dividiamo il nostro oggetto di risposta dal 0 al numero di limit inserito
+                }
+
+                if(personeFiltrate.length < 1){
+                    return res.status(200).json({success: true, data: []}) //Per gestire la ricerca che va a buon fine ma non trova nessun risultato
+                }
+
+                res.status(200).json(personeFiltrate) //se non abbiamo un valore di query specifico restituisci tutte le persone, res.status(200) che vuol dire che la ricerca è andata a buon fine.
+            })
+
+            app.listen(3000);
+*/
+
+/* Middleware:
+
+*/
+       
---
+++ pratica/persone.js
@@ -0,0 +1,64 @@
+const persone = [
+  {
+    id: "1",
+    nome: "Luca",
+    cognome: "Rossi",
+    eta: 25,
+    indirizzo: {
+      citta: "Milano",
+      via: "Via Torino",
+      cap: "48935"
+    },
+    interessi: ["basket", "calcio"]
+  },
+  {
+    id: "2",
+    nome: "Marco",
+    cognome: "Verdi",
+    eta: 21,
+    indirizzo: {
+      citta: "Torino",
+      via: "Via Milano",
+      cap: "97680"
+    },
+    interessi: ["pallavolo", "pallamano"]
+  },
+  {
+    id: "3",
+    nome: "Dino",
+    cognome: "Bianchi",
+    eta: 75,
+    indirizzo: {
+      citta: "Ferrara",
+      via: "Via Bologna",
+      cap: "44122"
+    },
+    interessi: ["bocce", "cantieri"]
+  },
+  {
+    id: "4",
+    nome: "Cordon",
+    cognome: "Blue",
+    eta: 54,
+    indirizzo: {
+      citta: "Messina",
+      via: "Via Catania",
+      cap: "54896"
+    },
+    interessi: ["mare", "vulcano"]
+  },
+  {
+    id: "5",
+    nome: "Dalia",
+    cognome: "Gialli",
+    eta: 23,
+    indirizzo: {
+      citta: "Napoli",
+      via: "Via Salerno",
+      cap: "12365"
+    },
+    interessi: ["cristo", "capri"]
+  }
+]
+
+module.exports = {persone}
